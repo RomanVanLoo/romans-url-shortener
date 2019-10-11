c2b008df6b7835534509344135778210cc55ced1
RomanVL <romanvanloo@icloud.com>
2019-10-11 21:17:49 +0200

    prototype is working

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,12 +4,21 @@ class LinksController < ApplicationController
   end
 
   def show
+    @link = Link.where(short_url: params[:short_url]).take
+    @link ? (redirect_to @link.long_url) : (redirect_to root_path)
   end
 
   def save
-    p "#"*80
-    p @link
-    p "#"*80
+    @link = Link.new(link_params)
+    @link.save
+    redirect_to root_path
+  end
+
+  def link_params
+    params.require(:link).permit(
+      :short_url,
+      :long_url
+    )
   end
 
 end
