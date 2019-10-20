e3fc7257847c75e7975c476e17de67fdfc996073
RomanVL <romanvanloo@icloud.com>
2019-10-20 12:00:16 +0200

    cleanup

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,14 +1,15 @@
 class Link < ApplicationRecord
   belongs_to :user, required: true
   has_many :clicks, dependent: :destroy
-  before_validation :smart_add_url_protocol
+
+  before_validation :add_url_protocol
+
   validates :short_url, uniqueness: true
   validates :long_url, uniqueness: true
 
   private
 
-  def smart_add_url_protocol
-    print "checking long url"
+  def add_url_protocol
     unless self.long_url[/\Ahttp:\/\//] || self.long_url[/\Ahttps:\/\//]
       self.long_url = "http://#{self.long_url}"
     end
